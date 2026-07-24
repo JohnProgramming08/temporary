33d942cb98191faf2284e196b60019e2e9fca224
Dylan Scully <dylan31scully@outlook.com>
2026-07-24 13:28:15 +0100

    feat: added an is_prime function

--- first.py
+++ first.py
@@ -1,3 +1,16 @@
 print("All even numbers between 2 and 100 inclusive.")
 for i in range(2, 101, 2):
     print(i)
+
+
+# Return whether or not a given number is prime
+def is_prime(n: int) -> bool:
+    if n % 2 == 0:
+        return False
+
+    end = int(n**0.5) + 1
+    for i in range(3, end, 2):
+        if n % i == 0:
+            return False
+
+    return n != 1
